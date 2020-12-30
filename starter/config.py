cdbfcf46ca1fcb0df0c3afd1950235a3888f1211
Plora Tran <ploratran@gmail.com>
2020-12-30 00:50:02 -0800

    define SQL database and storage account to deploy app to Azure portal:

--- starter/config.py
+++ starter/config.py
@@ -4,13 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or '[SQL_SERVER_GOES_HERE]'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or '[SQL_DATABASE_GOES_HERE]'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or '[SQL_USER_NAME_GOES_HERE]'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '[SQL_PASSWORD_GOES_HERE]'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'udacity-server123.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'udacity-db'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'uadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'mimimama0129@'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or '[BLOB_ACCOUNT_GOES_HERE]'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '[BLOB_STORAGE_KEY_GOES_HERE]'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or '[BLOB_CONTAINER_GOES_HERE]'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'udacity123'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'j7gozl06UxzYpMbNdddhI8MnzpqcW6s8AW717Xw4sA7FV3XF9nsQDWjqeVfN2rclVuKkcy1k14YjxzbCmWlZOg=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
