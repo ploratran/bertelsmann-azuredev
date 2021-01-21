77c977befa057b8b8f76044554bb384d57daab61
Plora Tran <ploratran@gmail.com>
2021-01-21 14:38:34 -0800

    successfully deployed to AZ App Service and Log in Log Stream

--- monitor-log-starter/FlaskExercise/__init__.py
+++ monitor-log-starter/FlaskExercise/__init__.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 wsgi_app = app.wsgi_app
 # TODO: Set the app's logger level to "warning"
 #   and any other necessary changes
+# Logging Level Doc: https://docs.python.org/3/library/logging.html
+app.logger.setLevel(logging.WARNING); # log level from warning and above
+
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler) # add stream hanlder to app.logger object
+
 
 import FlaskExercise.views
--- monitor-log-starter/FlaskExercise/views.py
+++ monitor-log-starter/FlaskExercise/views.py
@@ -7,6 +7,15 @@ def home():
     log = request.values.get('log_button')
     # TODO: Appropriately log the different button presses
     #   with the appropriate log level.
+    if log:
+        if log == 'info': 
+            app.logger.info('Info with no error log')
+        elif log == 'warning':
+            app.logger.warning('Warning')
+        elif log == 'error': 
+            app.logger.error('Error')
+        elif log == 'critical': 
+            app.logger.critical('Critical')
     return render_template(
         'index.html',
         log=log
